7e35509371f4a8e81ee75e9391230fe21e0fa1ea
naimulnobel <nobelnaimulislam@gmail.com>
2021-07-10 02:30:57 +0600

    props

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My firt react</p>
+        <Person name="rubel"/>
       </header>
     </div>
   );
+}
+function Person(props){
+  const personStyle= {
+    border:'2px solid red', 
+    margin:'10px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name: {props.name}</h1>
+    </div>
+  )
 }
 
 export default App;
